c5e6f682322a6f19442ac461e0bc98c225025371
Mikael Gråborg <mikael.graborg@iteam.se>
2018-06-24 13:19:45 +0200

    merge bets with games

--- utils.js
+++ utils.js
@@ -3,12 +3,11 @@ const axios = require('axios')
 const config = require('./config')
 
 async function getGames (token) {
-  console.log('lol')
-
-  const [games] = await Promise.all([
-    axios.get(`http://${config.serverUrl}/games`).then(res => res.data.games)
-    // axios.get(`http://${config.serverUrl}/bets`)
+  const [games, bets] = await Promise.all([
+    axios.get(`http://${config.serverUrl}/games`).then(res => res.data.games),
+    axios.get(`http://${config.serverUrl}/bets`)
   ])
+  console.log(bets)
   return games
   // return games.map(game => {
   //   Object.assign({}, game,
